15700dc3410675804c88262af956596298459665
TruongDX8 <truonghusk17@gmail.com>
2020-02-15 20:56:33 +0700

    Fix Order to Contract

--- NexusCommunicationSystem/Controllers/ContractsController.cs
+++ NexusCommunicationSystem/Controllers/ContractsController.cs
@@ -10,107 +10,107 @@ using NexusCommunicationSystem.Models;
 
 namespace NexusCommunicationSystem.Controllers
 {
-    public class OrdersController : Controller
+    public class ContractsController : Controller
     {
         private NexusCommunicationSystemContext db = new NexusCommunicationSystemContext();
 
-        // GET: Orders
+        // GET: Contracts
         public ActionResult Index()
         {
             return View(db.Orders.ToList());
         }
 
-        // GET: Orders/Details/5
+        // GET: Contracts/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Order order = db.Orders.Find(id);
-            if (order == null)
+            Contract contract = db.Orders.Find(id);
+            if (contract == null)
             {
                 return HttpNotFound();
             }
-            return View(order);
+            return View(contract);
         }
 
-        // GET: Orders/Create
+        // GET: Contracts/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Orders/Create
+        // POST: Contracts/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,OrderStatus,CreatedAt,UpdatedAt,SecurityDeposit,TotalAmount,PaidAmount,AmountDue,DateToLatestPayment,ChargeForReplacementDone,Discounts")] Order order)
+        public ActionResult Create([Bind(Include = "Id,OrderStatus,CreatedAt,UpdatedAt,SecurityDeposit,TotalAmount,AmountDue,Amount,NextPaymentAt,ChargeForReplacementDone,Discounts")] Contract contract)
         {
             if (ModelState.IsValid)
             {
-                db.Orders.Add(order);
+                db.Orders.Add(contract);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(order);
+            return View(contract);
         }
 
-        // GET: Orders/Edit/5
+        // GET: Contracts/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Order order = db.Orders.Find(id);
-            if (order == null)
+            Contract contract = db.Orders.Find(id);
+            if (contract == null)
             {
                 return HttpNotFound();
             }
-            return View(order);
+            return View(contract);
         }
 
-        // POST: Orders/Edit/5
+        // POST: Contracts/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,OrderStatus,CreatedAt,UpdatedAt,SecurityDeposit,TotalAmount,PaidAmount,AmountDue,DateToLatestPayment,ChargeForReplacementDone,Discounts")] Order order)
+        public ActionResult Edit([Bind(Include = "Id,OrderStatus,CreatedAt,UpdatedAt,SecurityDeposit,TotalAmount,AmountDue,Amount,NextPaymentAt,ChargeForReplacementDone,Discounts")] Contract contract)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(order).State = EntityState.Modified;
+                db.Entry(contract).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(order);
+            return View(contract);
         }
 
-        // GET: Orders/Delete/5
+        // GET: Contracts/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Order order = db.Orders.Find(id);
-            if (order == null)
+            Contract contract = db.Orders.Find(id);
+            if (contract == null)
             {
                 return HttpNotFound();
             }
-            return View(order);
+            return View(contract);
         }
 
-        // POST: Orders/Delete/5
+        // POST: Contracts/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Order order = db.Orders.Find(id);
-            db.Orders.Remove(order);
+            Contract contract = db.Orders.Find(id);
+            db.Orders.Remove(contract);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
