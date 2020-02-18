7c69c994b434c19ffd741b191c9a7fd6cefe073b
TruongDX8 <truonghusk17@gmail.com>
2020-02-18 21:43:23 +0700

    Seeding all other data

--- NexusCommunicationSystem/Models/Billing.cs
+++ NexusCommunicationSystem/Models/Billing.cs
@@ -10,11 +10,18 @@ namespace NexusCommunicationSystem.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
-        public string Account { get; set; } 
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public int ContractId { get; set; }
         public virtual Contract Contract { get; set; }
+        public Billing(){ }
+
+        public Billing(Contract contract)
+        {
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+            Contract = contract;
+            ContractId = contract.Id;
+        }
     }
 }
