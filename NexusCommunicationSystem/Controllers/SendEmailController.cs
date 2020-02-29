a56c15cae56276556582ac7172125ee9e4a6e946
TruongDX8 <truonghusk17@gmail.com>
2020-02-29 16:52:44 +0700

    SendEmail

---
+++ NexusCommunicationSystem/Controllers/SendEmailController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NexusCommunicationSystem.Controllers
+{
+    public class SendEmailController : Controller
+    {
+        // GET: SendEmail
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("lamvt39@gmail.com", "LamVuTung");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "cdnacyme";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 25,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+    }
+
+}
